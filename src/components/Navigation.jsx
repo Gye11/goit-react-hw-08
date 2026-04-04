692a76394d9551acad9773c408e4cb34619cdc1c
Gye11 <hilallornekk@gmail.com>
2026-04-05 02:18:22 +0300

    final

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,8 +3,12 @@ import { NavLink } from "react-router-dom";
 export default function Navigation() {
   return (
     <nav>
-      <NavLink to="/">Home</NavLink>
+      <NavLink to="/" end>
+        <Home />
+      </NavLink>
       <NavLink to="/contacts">Contacts</NavLink>
+      <NavLink to="/login">Login</NavLink>
+      <NavLink to="/register">Register</NavLink>
     </nav>
   );
 }
